f6c8a7a2d91be3b08ce174693f7b93407ba299b9
zzoo0123 <0123suh@gmail.com>
2024-02-22 21:10:30 +0900

    Auto stash before rebase of "origin/main"

--- HackerRank/Aggregation/Medium/Weather Observation Station 20.sql
+++ HackerRank/Aggregation/Medium/Weather Observation Station 20.sql
@@ -1,13 +1,26 @@
 -- A median is defined as a number separating the higher half of a data set from the lower half. 
 -- Query the median of the Northern Latitudes (LAT_N) from STATION and round your answer to 4 decimal places.
 
+-- 1
+SELECT ROUND(AVG(LAT_N),4)
+FROM (SELECT 
+        ROW_NUMBER() OVER (ORDER BY LAT_N) row_num -- 데이터 순위 매기기
+        , COUNT(*) OVER () n -- 데이터의 총 개수
+        , LAT_N
+    FROM STATION) t
+    WHERE CASE 
+    -- 데이터의 개수가 홀수인 경우
+    WHEN MOD(n,2) = 1 THEN row_num = (n+1)/2
+    -- 데이터의 개수가 짝수인 경우
+    ELSE row_num IN (n/2,(n/2)+1)
+END;
+
+-- 2
 SELECT 
-    ROUND(LAT_N, 4) AS Median_Latitude
-FROM (
-    SELECT LAT_N,
-        ROW_NUMBER() OVER (ORDER BY LAT_N) AS Row_Num,
-        COUNT(*) OVER () AS Total_Rows
-    FROM STATION
-) AS Subquery
+    ROUND(AVG(LAT_N),4)
+FROM (SELECT 
+        LAT_N
+        , PERCENT_RANK() OVER (ORDER BY LAT_N) p_rn
+    FROM STATION) t
 WHERE 
-    Row_Num IN ((Total_Rows + 1) / 2, (Total_Rows + 2) / 2);
+    p_rn = 0.5;
---
+++ HackerRank/Basic Join/Medium/The Report.sql
@@ -0,0 +1,20 @@
+-- Ketty gives Eve a task to generate a report containing three columns: Name, Grade and Mark. 
+-- Ketty doesn't want the NAMES of those students who received a grade lower than 8. 
+-- The report must be in descending order by grade -- i.e. higher grades are entered first. 
+-- If there is more than one student with the same grade (8-10) assigned to them, 
+-- order those particular students by their name alphabetically. 
+-- Finally, if the grade is lower than 8, 
+-- use "NULL" as their name and list them by their grades in descending order. 
+-- If there is more than one student with the same grade (1-7) assigned to them, 
+-- order those particular students by their marks in ascending order.
+
+SELECT
+    CASE WHEN g.Grade < 8 
+        THEN NULL
+        ELSE s.name
+        END AS Name,
+    g.Grade,
+    s.Marks
+FROM Students AS s 
+JOIN Grades AS g ON s.Marks BETWEEN g.MIN_MARK AND g.MAX_MARK
+ORDER BY g.Grade DESC, Name, Marks;
