c7bd34751097dc060e254855b04c75516dc06099
seunzu <0123suh@gmail.com>
2026-02-21 17:43:29 +0900

    leetcode/sql50/sortingAndGrouping

--- LeetCode/SQL 50/Sorting and Grouping/1045_Customers Who Bought All Products.sql
+++ LeetCode/SQL 50/Sorting and Grouping/1045_Customers Who Bought All Products.sql
@@ -1,13 +1,4 @@
-SELECT
-    S.product_id,
-    S.year AS first_year,
-    S.quantity,
-    S.price
-FROM Sales S
-JOIN (
-    SELECT product_id, MIN(year) AS first_year
-    FROM Sales
-    GROUP BY product_id
-) F
-    ON S.product_id = F.product_id
-    AND S.year = F.first_year;
+SELECT customer_id
+FROM Customer
+GROUP BY customer_id
+HAVING COUNT(DISTINCT product_key) = (SELECT COUNT(*) FROM Product);
